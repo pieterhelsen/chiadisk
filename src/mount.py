a7254e9d9ed2531e1bf7721141a1f51af5bd6d7a
pieterhelsen <doccie@gmail.com>
2021-06-07 21:22:53 +0200

    Fix bug where updating fstab would overwrite contents
    Also added additional logging

--- src/mount.py
+++ src/mount.py
@@ -36,22 +36,25 @@ class DiskMounter(ABC):
             self._disk.mount.mkdir(parents=True)
             logging.debug(f"Creating mount point: {self._disk.mount}")
 
-    def _update_fstab(self):
+    def _update_fstab(self) -> bool:
         fp = Path("/etc/fstab")
         has_duplicates = self._check_fstab_duplicates(fp)
-        total, used, free = self._disk.size
-        pretty_total = size(total, system=alternative)
-        defaults = "defaults,auto,users,rw,nofail,noatime 0 0"
-
-        fstr = (
-            "\n"
-            f"# {self._disk.model} - {pretty_total}\n"
-            f"UUID={self._disk.uuid}\t{self._disk.mount}\t{self._disk.format}\t{defaults}"
-        )
 
         if not has_duplicates:
+
+            total, used, free = self._disk.size
+            pretty_total = size(total, system=alternative)
+            defaults = "defaults,auto,users,rw,nofail,noatime 0 0"
+
+            fstr = (
+                "\n"
+                f"# {self._disk.model} - {pretty_total}\n"
+                f"UUID={self._disk.uuid}\t{self._disk.mount}\t{self._disk.format}\t{defaults}"
+            )
+
             if os.getuid() == 0:
-                fp.write_text(fstr, encoding="utf-8")
+                with fp.open('a', encoding='utf-8') as f:
+                    f.write(fstr)
                 logging.debug(f'Updated /etc/fstab for partition {self._disk.partition}')
             else:
                 logging.info('/etc/fstab could not be updated due to permission errors. '
@@ -75,11 +78,11 @@ class DiskMounter(ABC):
         res = False
 
         if re.search(str(self._disk.mount), txt):
-            logging.debug("Found mount point duplicate in /etc/fstab. Skipping...")
+            logging.debug(f"Found mount point duplicate in /etc/fstab ({self._disk.mount}). Skipping...")
             res = True
 
         if re.search(str(self._disk.uuid), txt):
-            logging.debug("Found UUID duplicate in /etc/fstab. Skipping...")
+            logging.debug(f"Found UUID duplicate in /etc/fstab ({self._disk.uuid}). Skipping...")
             res = True
 
         return res
